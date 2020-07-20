1def23afb468cf1e3fba2f9cbda1ea0e98b7d00f
Stephen Fiser <stephen@stephens-mbp.lan>
2020-07-20 18:12:12 -0500

    updates sr 3.3.0

--- app/reflexes/checklist_item_reflex.rb
+++ app/reflexes/checklist_item_reflex.rb
@@ -3,8 +3,12 @@
 class ChecklistItemReflex < ApplicationReflex
 
   def change_status
-    checklist_item = ChecklistItem.find(element.dataset.id)
+    checklist_item = ChecklistItem.includes(:checklist).find(element.dataset.id)
     checklist_item.update(status: element.dataset.status)
+    morph "#checklist-#{checklist_item.checklist_id}", ApplicationController.render(
+      partial: 'checklists/checklist',
+      locals: { checklist: checklist_item.checklist }
+    )
   end
 
 end
